5ea73e354d5c2a8454d51b1f0d706b167c2ae178
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-01-22 16:13:44 +0200

    CHUD methods added for reviews

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const productsRouter = require("./services/products");
+const reviewsRouter = require("./services/reviews");
 
 const server = express();
 
@@ -9,6 +10,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/products", productsRouter);
+server.use("/reviews", reviewsRouter);
 
 server.get("/", (req, res, next) => {
   res.send("Server is running!");
---
+++ src/services/reviews/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../db");
+
+const Model = require("../../model");
+
+const products = new Model("reviews");
+
+router.post("/", async (req, res, next) => {
+  try {
+    const response = await products.save(req.body);
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await products.findOne();
+    res.send(response.rows);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const productToDelete = products.findByIdAndDelete(req.params.id);
+    res.send(productToDelete);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const productToUpdate = products.findByIdAndUpdate(req.params.id, req.body);
+    res.send(productToUpdate);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+module.exports = router;
